99cbb2a68da5e6d2402a6a63eedb7a34d0e53aa7
Saksaran <saksaran.akra@kmutt.ac.th>
2023-03-22 22:10:21 +0700

    modified file

--- MyProject.java
+++ MyProject.java
@@ -1,20 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package myproject;
 
-/**
- *
- * @author vinze
- */
-public class MyProject {
 
-    /**
-     * @param args the command line arguments
-     */
+public class MyProject {
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello Worlds!");
     }
     
 }
